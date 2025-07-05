b0179607462a72825c4b297f3972d4450ae0e640
3xg3lin <krmutku76@gmail.com>
2025-07-05 17:27:28 +0300

    Created all nvim config
    	new file:   .config/nvim/init.lua
    	new file:   .config/nvim/lazy-lock.json
    	new file:   .config/nvim/lazy/lazy.nvim
    	new file:   .config/nvim/lua/config/lazy.lua
    	new file:   .config/nvim/lua/keymaps.lua
    	new file:   .config/nvim/lua/opts.lua
    	new file:   .local/share/nvim/lazy/cmp-buffer
    	new file:   .local/share/nvim/lazy/cmp-cmdline
    	new file:   .local/share/nvim/lazy/cmp-nvim-lsp
    	new file:   .local/share/nvim/lazy/cmp-path
    	new file:   .local/share/nvim/lazy/cmp-vsnip
    	new file:   .local/share/nvim/lazy/dial.nvim
    	new file:   .local/share/nvim/lazy/dressing.nvim
    	new file:   .local/share/nvim/lazy/lazy.nvim
    	new file:   .local/share/nvim/lazy/lua-utils.nvim
    	new file:   .local/share/nvim/lazy/neorg
    	new file:   .local/share/nvim/lazy/nui.nvim
    	new file:   .local/share/nvim/lazy/nvim-cmp
    	new file:   .local/share/nvim/lazy/nvim-lspconfig
    	new file:   .local/share/nvim/lazy/nvim-nio
    	new file:   .local/share/nvim/lazy/nvim-web-devicons
    	new file:   .local/share/nvim/lazy/pathlib.nvim
    	new file:   .local/share/nvim/lazy/plenary.nvim
    	new file:   .local/share/nvim/lazy/tokyonight.nvim
    	new file:   .local/share/nvim/lazy/treesj
    	new file:   .local/share/nvim/lazy/vim-startuptime
    	new file:   .local/share/nvim/lazy/vim-vsnip
    	new file:   .local/share/nvim/lazy/which-key.nvim
    	new file:   .local/share/nvim/shada/main.shada
    	modified:   .local/share/nvim/lazy/pathlib.nvim (untracked content)
    	modified:   .local/share/nvim/lazy/treesj (untracked content)

---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,113 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    -- { import = "plugins" },
+    {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  -- I have a separate config.mappings file where I require which-key.
+  -- With lazy the plugin will be automatically loaded when it is required somewhere
+  { "folke/which-key.nvim", lazy = true },
+
+  {
+    "nvim-neorg/neorg",
+    -- lazy-load on filetype
+    ft = "norg",
+    -- options for neorg. This will automatically call `require("neorg").setup(opts)`
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+      },
+    },
+  },
+
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      -- ...
+    end,
+  },
+
+  -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
+  -- So for api plugins like devicons, we can always set lazy=true
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+  -- you can use the VeryLazy event for things that can
+  -- load later and are not important for the initial UI
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
+
+  {
+    "Wansmer/treesj",
+    keys = {
+      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
+
+  {
+    "monaqa/dial.nvim",
+    -- lazy-load on keys
+    -- mode is `n` by default. For more advanced options, check the section on key mappings
+    keys = { "<C-a>", { "<C-x>", mode = "n" } },
+  },
+}
+  -- local plugins need to be explicitly configured with dir
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  -- install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  -- checker = { enabled = true },
+})
